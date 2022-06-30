1e2f2de5d5c72597acedc8861b372ab15effaf02
Lukas Berghegger <lukasberghegger@gmail.com>
2022-06-30 16:21:20 +0200

    .

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-    public List<GameObject> Enemies;
-    public List<Vector3> SpawnPoints;
+	public List<GameObject> Enemies;
+	public List<Vector3> SpawnPoints;
 
-    private List<GameObject> InstancedEnemies = new List<GameObject>();
+	private List<GameObject> InstancedEnemies = new List<GameObject>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+	   
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (InstancedEnemies.Count == 0 && Random.Range(0.0f, 1.0f) > 0.5f && SpawnPoints.Count > 0)
-        {
-            InstancedEnemies.Add(Enemies[Random.Range(0, Enemies.Count)]);
-            Instantiate(InstancedEnemies[^1], SpawnPoints[Random.Range(0, SpawnPoints.Count - 1)], Quaternion.identity);
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (InstancedEnemies.Count == 0 && Random.Range(0.0f, 1.0f) > 0.5f && SpawnPoints.Count > 0)
+		{
+			InstancedEnemies.Add(Enemies[Random.Range(0, Enemies.Count)]);
+			Instantiate(InstancedEnemies[^1], SpawnPoints[Random.Range(0, SpawnPoints.Count - 1)], Quaternion.identity);
+		}
+	}
 }
